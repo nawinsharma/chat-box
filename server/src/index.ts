e09b115bbff94c2b0127330e6651341c2525288e
Nawin <nawinsharma60@gmail.com>
2024-11-04 04:02:20 +0530

    auth route done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { createServer } from "http";
+import Routes from "./routes/index.js";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8080;
@@ -15,5 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req: Request, res: Response) => {
   return res.send("It's working Guys 🙌");
 });
+app.use("/api", Routes);
 
 server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
